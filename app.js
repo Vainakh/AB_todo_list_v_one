1416680e30b89182028cbd44edc9e0860f8cdaf5
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-04-29 06:49:09 -0700

    Added templates and layout

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.set("view engine", "ejs");
 
@@ -25,7 +26,7 @@ app.get("/", function(req, res){
 
   let day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { kindOfDay: day, newListItems: items });
+  res.render("list", { listTitle: day, newListItems: items });
 
 });
 
@@ -33,11 +34,28 @@ app.post("/", function(req, res){
 
   let item = req.body.newItem;
 
-  items.push(item);
-
-  res.redirect("/");
-
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 });
+
+app.get("/work", function(req, res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems})
+})
+
+app.post("/work", function(req, res){
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+})
+
+app.get("/about", function(req, res){
+  res.render("about");
+})
 
 app.listen(3000, function(){
   console.log("Server started on port 3000");
